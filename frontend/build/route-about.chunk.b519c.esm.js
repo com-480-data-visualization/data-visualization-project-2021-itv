0ed8b9f19589427def41c77098c055fd102ffbde
Amadeous <julien@biefer.me>
2021-05-05 14:51:20 +0200

    Add build

---
+++ frontend/build/route-about.chunk.b519c.esm.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{VDfu:function(o){o.exports={about:"about__3Ycn6"}},ZbNB:function(o,t,n){"use strict";n.r(t);var u=n("hosL"),e=n("VDfu"),c=n.n(e);t.default=()=>Object(u.h)("div",{class:c.a.about},Object(u.h)("h1",null,"About"),Object(u.h)("p",null,"More information on this project here."))}}]);
+//# sourceMappingURL=route-about.chunk.b519c.esm.js.map
