84cec4bacc2797f4867a5d473fc612db41a03232
Zyf <zyfarok@gmail.com>
2021-05-07 14:02:43 +0200

    few improvement in website text

--- website/src/routes/beatmaps/index.js
+++ website/src/routes/beatmaps/index.js
@@ -27,14 +27,16 @@ const Beatmaps = () => (
 					<div class={style.info}>Creator: beatmap creator</div>
 					<div class={style.info}>Created: beatmap creation date</div>
 					<div class={style.features}>
-						<Slider label="feature1" min="1" max="100" value="50"/>
-						<Slider label="feature2" min="1" max="100" value="30"/>
-						<Slider label="feature3" min="1" max="100" value="70"/>
+						<div class={style.info}>Select score parameters :</div>
+						<Slider label="min. accuracy" min="1" max="100" value="90"/>
+						<Slider label="min. max-combo" min="1" max="100" value="90"/>
+						<Slider label="max. misses" min="1" max="100" value="10"/>
 					</div>
 				</div>
 			<div class={style.right}>
 				<h2>How difficult is this beatmap?</h2>
-				<p>The figure below displays the probability that the player achieves the score on the given beatmap.
+				<p>The figure below displays the probability that the player achieves some score on the given beatmap.
+				You can change the score parameters on the left. 
 				This is very useful to estimate the difficulty of the beatmap.</p>
 				<img src={lineChart}/>
 			</div>
